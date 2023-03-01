e7aacbac2f4f768d0dd88d4fbe44a4e1a99f8b53
”airbloodz1” <airbloodz1@gmail.com>
2023-03-01 17:57:20 +0200

    Initial commit

--- main.py
+++ main.py
@@ -26,60 +26,67 @@ if os.access(file_path, os.F_OK) == True:
     print("{0} перемещается в нужное место, {1}".format(file_path, new_location))
 
     sheets = {
-        "Kvitneve |Result_Forecast_Data": 2082,
-        "Peremoga | Result_Forecast_Data": 2081,
-        "Lviv | Result_Forecast_Data": 2930,
-        "Odesa |Result_Forecast_ Data": 2102,
-        "Zaporizhja|Result_Forecast_Data": 2135
+         "Kvitneve |Result_Forecast_Data": 2082,
+         "Peremoga | Result_Forecast_Data": 2081,
+         "Lviv | Result_Forecast_Data": 2930,
+         "Odesa |Result_Forecast_ Data": 2102,
+         "Zaporizhja|Result_Forecast_Data": 2135
     }
     # Set up SQL connection
     engine = create_engine('mssql+pyodbc://s-kv-center-s64/PlanRC?driver=SQL+Server')
+    # Получить список всех листов в файле Excel
+    xls = pd.ExcelFile(file_path)
+    sheet_names = xls.sheet_names
+
 
     # Цикл для обработки данных по каждому листу
     for sheet, idrc in sheets.items():
 
-        # Read the excel file and set the correct sheet name and header rows
-        df = pd.read_excel(file_path, sheet_name=sheet, header=4)
-        # Извлечение названий столбцов из датафрейма
-        column_names = df.columns.tolist()
-        # Создание нового датафрейма с названиями столбцов
-        columns_df = pd.DataFrame(column_names, columns=["column_name"])
+        for sh in sheet_names:
 
-        # добавление столбца счетчика строк до загрузки в SQL
-        columns_df = columns_df.iloc[1:, ]
-        columns_df['ID'] = [i for i in range(1, len(columns_df) + 1)]
-        columns_df['RCID'] = idrc
+            if sheet  in sh:
 
-        # Создание нового датафрейма с названиями столбцов
-        df_type= pd.read_excel(file_path, sheet_name=sheet, header=2)
-        # Извлечение названий столбцов из датафрейма
-        column_type = df_type.columns.tolist()
-        # добавление столбца  строк до загрузки в SQL
-        typpe = pd.DataFrame(column_type, columns=["Type"])
-        column_type = df_type.iloc[1:, ]
-        columns_df['Type'] = typpe
+                # Read the excel file and set the correct sheet name and header rows
+                df = pd.read_excel(file_path, sheet_name=sheet, header=4)
+                # Извлечение названий столбцов из датафрейма
+                column_names = df.columns.tolist()
+                # Создание нового датафрейма с названиями столбцов
+                columns_df = pd.DataFrame(column_names, columns=["column_name"])
 
-        # Создание нового датафрейма с названиями столбцов
-        df_Total = pd.read_excel(file_path, sheet_name=sheet, header=0)
-        column_Total = df_Total.columns.tolist()
-        Total_Data = pd.DataFrame(column_Total, columns=["Total_Data"])
-        columns_df['Total_Data'] = Total_Data
+                # добавление столбца счетчика строк до загрузки в SQL
+                columns_df = columns_df.iloc[1:, ]
+                columns_df['ID'] = [i for i in range(1, len(columns_df) + 1)]
+                columns_df['RCID'] = idrc
 
-        # Загрузка названий столбцов в SQL
-        columns_df.to_sql("_Ant_PlanRM_Column_Name", engine, if_exists="replace", index=False)
+                # Создание нового датафрейма с названиями столбцов
+                df_type= pd.read_excel(file_path, sheet_name=sheet, header=2)
+                # Извлечение названий столбцов из датафрейма
+                column_type = df_type.columns.tolist()
+                # добавление столбца  строк до загрузки в SQL
+                typpe = pd.DataFrame(column_type, columns=["Type"])
+                column_type = df_type.iloc[1:, ]
+                columns_df['Type'] = typpe
 
-        # Загрузка данных в SQL таблицу
-        df.to_sql("_Ant_PlanRM_Data_ImportExcel", engine, if_exists="replace", index=False)
+                # Создание нового датафрейма с названиями столбцов
+                df_Total = pd.read_excel(file_path, sheet_name=sheet, header=0)
+                column_Total = df_Total.columns.tolist()
+                Total_Data = pd.DataFrame(column_Total, columns=["Total_Data"])
+                columns_df['Total_Data'] = Total_Data
 
-        p = ProcExec(r'D:\Заливка Плана РМ\проверка_данных.xlsm')
-        p.run_macro("Module1.Execute_command")
-        p.save_and_close()
+                # Загрузка названий столбцов в SQL
+                columns_df.to_sql("_Ant_PlanRM_Column_Name", engine, if_exists="replace", index=False)
 
+                # Загрузка данных в SQL таблицу
+                df.to_sql("_Ant_PlanRM_Data_ImportExcel", engine, if_exists="replace", index=False)
 
-        print(sheet)
+                p = ProcExec(r'D:\Заливка Плана РМ\проверка_данных.xlsm')
+                p.run_macro("Module1.Execute_command")
+                p.save_and_close()
 
-else:
-    print("Файл не найден")
+                print(sheet)
 
-
-
+            else:
+                print(f"Лист {sheet} не найден в файле {file_path}. Пропускаем его обработку.")
+                continue
+    else:
+        print("Файл не найден")
